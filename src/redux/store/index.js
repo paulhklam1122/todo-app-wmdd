0ad46eed4fe522dc08a4d2f6718220fb57e44fbf
Paul Lam <paulhklam1122@gmail.com>
2019-03-11 11:09:56 -0700

    update all npm packages and update code for redux middleware

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
-import promiseMiddleWare from 'redux-promise-middleware'
+import promise from 'redux-promise-middleware'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import todoApp from '../reducers'
 
-const middleWare = applyMiddleware(promiseMiddleWare(), thunk, logger)
+const middleWare = applyMiddleware(promise, thunk, logger)
 
 const store = createStore(todoApp, composeWithDevTools(middleWare))
 
